8371bb296ae2f075e7874a677437e230e27e90d8
Malvika-Grover <malvikagrover22@gmail.com>
2021-06-24 22:28:32 +0530

    Reverse Number Program

---
+++ src/main/java/grover/malvika/java/ReverseNumber.java
@@ -0,0 +1,29 @@
+package grover.malvika.java;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        int number = 1112;
+        int reversed = 0;
+
+        while (number!=0) {
+            int digit = number % 10;                    //2     -> 1    -> 1     -> 1
+            reversed = reversed * 10 + digit;           //2     -> 21   -> 211   -> 2111
+            number = number/10;                         //111   -> 11   -> 1     -> 0
+        }
+        System.out.println(reversed);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
