9f25ad562733cd183ecd7140b1c21645b5b414aa
Malvika-Grover <malvikagrover22@gmail.com>
2021-06-24 22:29:57 +0530

    Swap 2 numbers Program

---
+++ src/main/java/grover/malvika/java/SwapTwoNumber.java
@@ -0,0 +1,21 @@
+package grover.malvika.java;
+
+public class SwapTwoNumber {
+
+    public static void swap(int a, int b){
+        System.out.println("Numbers before swap "+a+" "+b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("Numbers after swap "+a+" "+b);
+    }
+
+
+    public static void main(String[] args) {
+
+        swap(2,3);
+
+    }
+
+
+}
