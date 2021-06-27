74f57e16ad6649cee721c95d7cf384b93711180e
Malvika-Grover <malvikagrover22@gmail.com>
2021-06-27 13:47:49 +0530

    (This is type-3 ListIterator)

---
+++ src/main/java/grover/malvika/java/CursorsListIterator.java
@@ -0,0 +1,46 @@
+package grover.malvika.java;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class CursorsListIterator {
+    public static void main(String[] args) {
+        //Generic syntax
+        ArrayList<String> data = new ArrayList<>();
+
+        //Adding data to ArrayList
+        data.add("Malvika");
+        data.add("Grover");
+        data.add("Shubham");
+        data.add("Narendra");
+        data.add("Sunita");
+
+        System.out.println(data);
+
+        //Created a new object of listeIterator using method "listIterator()"
+        ListIterator li = data.listIterator();
+        while (li.hasNext()){
+
+            //To iterate over List
+            String item = (String) li.next();
+
+            //add() method
+            if (item.equalsIgnoreCase("Shubham")){
+                li.add("Grover");
+            }
+
+            //set() method
+            if (item.equalsIgnoreCase("Narendra")){
+                li.set("Narendra Kumar");
+            }
+
+            //remove() method
+            if (item.equalsIgnoreCase("Grover")){
+                li.remove();
+            }
+
+        }
+        System.out.println(data);
+
+    }
+}
